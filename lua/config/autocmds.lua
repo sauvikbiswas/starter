4a9b8f1087dd22841e4dbdf52611e3c168950b49
Sauvik Biswas <sauvik.biswas@gmail.com>
2024-11-08 14:25:51 +0530

    switched to go.nvim

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,24 +1,24 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-vim.api.nvim_create_autocmd("BufWritePost", {
-  group = vim.api.nvim_create_augroup("BufFormatter", { clear = true }),
-  pattern = "*.proto",
-  callback = function()
-    local filename = vim.api.nvim_buf_get_name(0)
-    vim.fn.jobstart({ "buf", "format", filename, "-w" }, {
-      stdout_buffered = true,
-      on_stdout = function(_, data)
-        if data then
-          vim.cmd("edit")
-          print(data)
-        end
-      end,
-      on_stderr = function(_, data)
-        if data then
-          print(data)
-        end
-      end,
-    })
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+--   group = vim.api.nvim_create_augroup("BufFormatter", { clear = true }),
+--   pattern = "*.proto",
+--   callback = function()
+--     local filename = vim.api.nvim_buf_get_name(0)
+--     vim.fn.jobstart({ "buf", "format", filename, "-w" }, {
+--       stdout_buffered = true,
+--       on_stdout = function(_, data)
+--         if data then
+--           vim.cmd("edit")
+--           print(data)
+--         end
+--       end,
+--       on_stderr = function(_, data)
+--         if data then
+--           print(data)
+--         end
+--       end,
+--     })
+--   end,
+-- })
--- lua/plugins/gopher.lua
+++ lua/plugins/gopher.lua
@@ -1,10 +1,14 @@
 return {
-  "olexsmir/gopher.nvim",
-  ft = "go",
-  config = function(_, opts)
-    require("gopher").setup(opts)
+  "ray-x/go.nvim",
+  dependencies = { -- optional packages
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("go").setup()
   end,
-  build = function()
-    vim.cmd([[silent! GoInstallDeps]])
-  end,
+  event = { "CmdlineEnter" },
+  ft = { "go", "gomod" },
+  build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 }
